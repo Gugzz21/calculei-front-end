cccd659f6d7ffc403a5c3948a9f927167417f8e0
Gugzz21 <gustavodinizz966@gmail.com>
2026-03-31 13:31:51 -0300

    Melhora do código do componente de Data.

--- src/components/CentralCard/Data.tsx
+++ src/components/CentralCard/Data.tsx
@@ -8,17 +8,11 @@ interface DataProps {
 
 function Data({ title, value, onChange, max, min }: DataProps) {
   return (
-<<<<<<< HEAD
-    <div className="flex flex-col gap-1">
-      <strong className="text-[13px] text-gray-700 font-semibold">
-        {props.title}
-      </strong>
-      <div className="bg-white border border-blue-400 w-[180px] h-[45px] flex items-center px-3 rounded-md">
-=======
     <div className="flex flex-col gap-1 w-full">
-      <strong className="text-[14px] text-gray-700 font-semibold">{title}</strong>
+      <strong className="text-[14px] text-gray-700 font-semibold">
+        {title}
+      </strong>
       <div className="bg-white border border-blue-400 w-full md:w-[180px] h-[45px] flex items-center px-3 rounded-md">
->>>>>>> testeEndpoint1
         <input
           type="date"
           value={value}
