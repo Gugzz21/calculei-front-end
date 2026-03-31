5172e8b9eb1d0a822a68e4304c6bda40929dc26a
Gugzz21 <gustavodinizz966@gmail.com>
2026-03-31 12:46:28 -0300

    AAAAAAAAAAAAAAAAA

--- src/components/CentralCard/Data.tsx
+++ src/components/CentralCard/Data.tsx
@@ -1,7 +1,9 @@
 function Data(props: any) {
   return (
     <div className="flex flex-col gap-1">
-      <strong className="text-[13px] text-gray-700 font-semibold">{props.title}</strong>
+      <strong className="text-[13px] text-gray-700 font-semibold">
+        {props.title}
+      </strong>
       <div className="bg-white border border-blue-400 w-[180px] h-[45px] flex items-center px-3 rounded-md">
         <input
           type="date"
